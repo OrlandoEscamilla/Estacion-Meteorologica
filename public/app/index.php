ec08d7abedfc10ecbf6802957ed554bd4c5b11d8
jmanuelRC <juan.02324@gmail.com>
2019-11-10 17:18:16 -0600

    mini api

---
+++ public/app/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+	<title>Real Time Temperature</title>
+</head>
+<body>
+	<div id="mainContainer"></div>
+</body>
+</html>
