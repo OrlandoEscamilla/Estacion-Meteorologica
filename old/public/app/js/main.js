8a3261813d5bb8d70f64804368ee2ff2889fd2d2
jmanuelRC <juan.02324@gmail.com>
2019-12-07 11:48:11 -0600

    change project structure

---
+++ old/public/app/js/main.js
@@ -0,0 +1,6 @@
+function 
+
+document.onload = function () {
+	initDOM();
+	initData();
+};
---
+++ old/src/Controller/Temperature.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace EstacionMeteorologica\Controller;
+
+class Temperature
+{
+	static function getTemperature($data)
+	{
+		
+	}
+}
